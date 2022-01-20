4683f3c44b02015b687dd1d418e8f928a83034f0
Stanislav Georgiev <sgeorgiev@appolica.com>
2022-01-20 23:07:36 +0200

    Add empty home screen and setup navigation
    Signed-off-by: Stanislav Georgiev <sgeorgiev@appolica.com>

---
+++ app/src/main/java/com/github/sgeorgiev/randompoems/ui/home/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.github.sgeorgiev.randompoems.ui.home
+
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home")
+}
